35e7a8f8b5ff0aacc511536397175b7ec5e2de7f
Sayed Mohammad Moosavi <mousavi.mohammad97@yahoo.com>
2023-04-21 18:08:13 +0330

    Products API

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <p>Products</p>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="item in products">
+        <NuxtLink :to="`/products/${p.id}`">{{ item.title }}</NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+//Fetch the Products
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+</script>
 
 <style scoped></style>
