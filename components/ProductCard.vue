5495f03a0e9c0c632883e936c2335f501b98bc67
Sayed Mohammad Moosavi <mousavi.mohammad97@yahoo.com>
2023-04-22 15:57:38 +0330

    Single Product Page added

---
+++ components/ProductCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <NuxtLink :to="`/products/${item.id}`">
+    <div
+      class="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80"
+    >
+      <img
+        :src="item.image"
+        :alt="item.title"
+        class="h-full w-full object-cover object-center lg:h-full lg:w-full"
+      />
+    </div>
+    <div class="mt-4 flex justify-between">
+      <div>
+        <h3 class="text-sm text-gray-700">
+          <span aria-hidden="true" class="absolute inset-0" />
+          {{ item.title }}
+        </h3>
+        <p class="mt-1 text-sm text-gray-500">{{ item.category }}</p>
+      </div>
+      <p class="text-sm font-medium text-gray-900">${{ item.price }}</p>
+    </div>
+  </NuxtLink>
+</template>
+
+<script setup>
+const { item } = defineProps(["item"]);
+</script>
+
+<style scoped></style>
